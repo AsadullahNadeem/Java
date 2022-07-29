877a547cf75ae3ba260739e370a9d801dccc57ff
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-07-29 09:56:52 -0700

    update

--- java_rest_Course/Lecture1/Lecture1.java
+++ java_rest_Course/Lecture1/Lecture1.java
@@ -1,1 +1,18 @@
-Class
+public class Lecture1{
+    public static void main(String[] args) {
+        System.out.println("Lecture 1");
+        int a = 0;
+        for(int i = 0; i < 10; i++){                                                                                        
+            System.out.println("a"+i);
+            a++;
+
+            }
+            System.out.println("Lecturem");
+            int a = 0;
+            for(int i = 0; i < 10; i++){
+                System.out.println("a"+i);
+    
+        }
+    }
+
+}
