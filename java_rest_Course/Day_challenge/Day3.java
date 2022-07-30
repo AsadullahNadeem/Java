ef72a78878088ac114b35f74c9a1d63debb08acd
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-07-30 11:05:50 +0530

    update

--- java_rest_Course/Day_challenge/Day3.java
+++ java_rest_Course/Day_challenge/Day3.java
@@ -1,2 +1,58 @@
-public class Day3 {
+import java.util.Scanner;
+
+class lecture{
+    public void lec(){
+        System.out.println("Write Number Adult or not Adult");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age > 18){
+            System.out.println(" Adult " + age);
+        }else {
+            System.out.println("not Adult " + age);
+        }
+    }
+}
+class lecturn{
+    public void len(){
+        System.out.println("Write Number Even or odd");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x % 2 == 0){
+            System.out.println(" Even " + x);
+        }else{
+            System.out.println(" odd " + x);
+        }
+    }
+}
+
+class lecturm{
+    public void lem() {
+        System.out.println("Write Number greater than lesser");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == b){
+            System.out.println("Equal");
+        }else {
+            if (a > b) {
+                System.out.println("A is Greater " +  a);
+            }else {
+                System.out.println("A is Lesser " +  a);
+            }
+        }
+
+    }
+}
+
+
+class Day3{
+    public static void main(String[] args) {
+        lecture lec = new lecture();
+        lecturn len = new lecturn();
+        lecturm lem = new lecturm();
+
+        lec.lec();
+        len.len();
+        lem.lem();
+    }
 }
---
+++ java_rest_Course/Day_challenge/Day3Button.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day3Button {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        if (button == 1)
+        {
+            System.out.println("hello");
+        }
+        else if (button == 2){
+            System.out.println("Bonjour");
+        }
+        else if (button == 3){
+            System.out.println("Namaste");
+        }
+        else if (button == 4)
+        {
+            System.out.println("bye");
+        }
+        else {
+            System.out.println("invalid");
+        }
+    }
+}
+
