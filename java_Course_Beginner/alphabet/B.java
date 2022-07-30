58775ecffb8bb8c66351ca792f853dcaf9f4bf7e
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-07-31 02:17:34 +0530

    update

--- java_Course_Beginner/alphabet/B.java
+++
@@ -1,6 +0,0 @@
-package alphabet;
-
-public class B {
-    char b;
-    for(b = 'a'; )
-}
--- java_rest_Course/Day_challenge/Day6.java
+++ java_rest_Course/Day_challenge/Day6.java
@@ -15,7 +15,7 @@ class leac{
                 "   ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ \n" +
                 "  / __/ _` | |/ __| | | | |/ _` | __/ _ \\| '__|\n" +
                 " | (_| (_| | | (__| |_| | | (_| | || (_) | |   \n" +
-                "  \\___\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \n");
+                "  \\___\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|\n");
       System.out.println(" Type Any Number\n +(1)\n *(2)\n -(3)\n /(4)\n %(5)\n");
         Scanner sc = new Scanner(System.in);
         int button = sc.nextInt();
@@ -47,6 +47,22 @@ class leac{
             f = sc.nextInt();
             Sub  = e-f;
             System.out.println("Subtraction:" + Sub);
+        } else if (button == 4) {
+            System.out.println("Type a number");
+            g = sc.nextInt();
+            System.out.println("Type Another Number");
+            h = sc.nextInt();
+            divis = g /h;
+            System.out.println("Division:" + divis);
+        } else if (button == 5) {
+            System.out.println("Type a number");
+            i = sc.nextInt();
+            System.out.println("Type Another Number");
+            j = sc.nextInt();
+            rem = i%j;
+            System.out.println("Modulo or remainder:" + rem);
+        }else {
+            System.out.println("ERROR");
         }
     }
 }
