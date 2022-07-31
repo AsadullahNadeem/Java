8875642f0b2e9d4adb74a9f6cfe033d29b6bcdb2
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-07-31 17:20:45 +0530

    update

--- java_rest_Course/Day_challenge/Day8.java
+++ java_rest_Course/Day_challenge/Day8.java
@@ -1,11 +1,15 @@
 public class Day8 {
     public static void main(String[] args) {
-        System.out.println("hello World ");
+        //System.out.println("hello World ");
         //loop
         //while loop
         //do while loop
-        System.out.println("hello World ");
-        System.out.println("hello World ");
-        System.out.println("hello World ");
+        //System.out.println("hello World ");
+       // System.out.println("hello World ");
+       // System.out.println("hello World ");
+        for (int counter =0; counter < 3; counter = counter + 1 )
+        {
+            System.out.println("haii");
+        }
     }
 }
