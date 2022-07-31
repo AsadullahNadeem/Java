bc935ddb0a1d9e90a566524b721b2e056af94891
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-07-31 17:17:46 +0530

    update class Data8.java

--- java_rest_Course/Day_challenge/Day8.java
+++ java_rest_Course/Day_challenge/Day8.java
@@ -1,5 +1,11 @@
 public class Day8 {
     public static void main(String[] args) {
-        
+        System.out.println("hello World ");
+        //loop
+        //while loop
+        //do while loop
+        System.out.println("hello World ");
+        System.out.println("hello World ");
+        System.out.println("hello World ");
     }
 }
