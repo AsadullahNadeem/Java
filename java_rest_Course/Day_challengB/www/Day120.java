77dd40f84d68ee5f6de0832d2e56ea8a8d9cdafe
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-08-02 10:25:00 +0530

    update

---
+++ java_rest_Course/Day_challengB/www/Day120.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day120 {
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                "                                                                                                                \n" +
+                "                                _/      _/                                _/        _/  _/  _/                      \n" +
+                "         _/_/_/    _/    _/  _/_/_/_/  _/_/_/      _/_/    _/_/_/        _/  _/        _/  _/    _/_/    _/  _/_/   \n" +
+                "        _/    _/  _/    _/    _/      _/    _/  _/    _/  _/    _/      _/_/      _/  _/  _/  _/_/_/_/  _/_/        \n" +
+                "       _/    _/  _/    _/    _/      _/    _/  _/    _/  _/    _/      _/  _/    _/  _/  _/  _/        _/           \n" +
+                "      _/_/_/      _/_/_/      _/_/  _/    _/    _/_/    _/    _/      _/    _/  _/  _/  _/    _/_/_/  _/            \n" +
+                "     _/              _/                                                                                             \n" +
+                "    _/          _/_/                                                                                                \n");
+        System.out.println(" Python killer code, editor and view the result. ");
+    python1 print = new python1();
+    print.print();
+    }
+}
+class python1{
+    void print(){
+    }
+}
--- java_rest_Course/Day_challenge/Day8.java
+++ java_rest_Course/Day_challenge/Day8.java
@@ -1,19 +1,51 @@
+import java.util.Scanner;
+
 public class Day8 {
     public static void main(String[] args) {
+
         //System.out.println("hello World ");
+
         //loop
         //while loop
         //do while loop
+
         //System.out.println("hello World ");
        // System.out.println("hello World ");
        // System.out.println("hello World ");
+
         //for (int counter =0; counter < 3; counter = counter + 1 )
         //{
         //    System.out.println("haii");
      //   }
-            for (int co = 0; co < 11; co++)
-            {
-                System.out.print( co + " ");
-            }
+         //   for (int co = 0; co < 11; co++)
+          //  {
+           //     System.out.print( co + " ");
+        //   }
+
+       // int i = 0;
+        // while ( i < 11 )
+        // {
+           // System.out.println(i);
+            // i=i+1;
+        //}
+
+
+       // Scanner sc = new Scanner(System.in);
+       // int n = sc.nextInt();
+//
+      // //int sum = 0;
+       // //for (int i=0; i<=n; i++)
+        //{
+          //  sum = sum + i;
+        ////}
+        //System.out.println(sum);
+
+
+       // Scanner sc = new Scanner(System.in);
+       // int n = sc.nextInt();
+      //  for (int i=1; i<11; i++)
+      //  {
+      //      System.out.println(i);
+      //  }
     }
 }
