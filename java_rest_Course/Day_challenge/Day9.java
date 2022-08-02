306314577047ede1d2bb62210eebf077980437fb
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-08-02 17:37:23 +0530

    update Day9

--- java_rest_Course/Day_challenge/Day9.java
+++ java_rest_Course/Day_challenge/Day9.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 public class Day9
 {
-    
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                "             _..._                          .-'''-.                               \n" +
+                "      _   .-'_..._''.                      '   _    \\                     _       \n" +
+                "    .' ).' .'      '.\\                   /   /` '.   \\                   ( `.     \n" +
+                "   / .'/ .'                             .   |     \\  '                _.._'. \\    \n" +
+                "  / / . '                            .| |   '      |  '             .' .._| \\ \\   \n" +
+                " / /  | |                          .' |_\\    \\     / /              | '      \\ \\  \n" +
+                ". '   | |                        .'     |`.   ` ..' /             __| |__     ' . \n" +
+                "| |   . '                       '--.  .-'   '-...-'`             |__   __|    | | \n" +
+                "' '    \\ '.          .             |  |                             | |       ' ' \n" +
+                " \\ \\    '. `._____.-'/             |  |                             | |      / /  \n" +
+                "  \\ \\     `-.______ /              |  '.'                           | |     / /   \n" +
+                "   \\ '.            `               |   /                            | |   .' /    \n" +
+                "    '._)                           `'-'                             |_|  (_.'     \n");
+        System.out.println("      Celsius to Fahrenheit (C to F).");
+        celsius main = new celsius();
+        main.main();
+    }
+}
+class celsius{
+    void main(){
+
+    }
 }
