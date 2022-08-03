e1fee3c45fc4007db12beb4a40e019d978d559f0
Asadullah nadeem <88024587+AsadullahNadeem@users.noreply.github.com>
2022-08-03 07:46:37 +0530

    update day11

--- java_rest_Course/Day_challenge/Day12.java
+++ java_rest_Course/Day_challenge/Day12.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Day12 {
+    public static void main(String[] args) {
+        int mark= 100;
+        String result;
+
+
+        result = mark>10 ? "Pass" : "Fail";
+
+        System.out.println(result);
+    }
 }
